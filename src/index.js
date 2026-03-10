0964772704b52098d10297ba03b7984aa75937f4
qile <98936712+csdropout@users.noreply.github.com>
2026-03-10 16:27:33 +0800

    Load tabs on tab clicks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import loadHome from "./load-home.js";
+import loadMenu from "./load-menu.js";
+import loadAbout from "./load-about.js";
+
+const tabs = document.querySelectorAll(".tab");
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        // const target = tab.dataset.tab;
+        tabs.forEach((t) => t.classList.remove('active'));
+        tab.classList.add('active');
+        // document.getElementById(target).classList.add('active');
+    });
+})
+
+const homeTab = document.querySelector("#home");
+homeTab.addEventListener("click", loadHome);
+const menuTab = document.querySelector("#menu");
+menuTab.addEventListener("click", loadMenu);
+const aboutTab = document.querySelector("#about");
+aboutTab.addEventListener("click", loadAbout);
 
 loadHome();
+homeTab.classList.add("active");
